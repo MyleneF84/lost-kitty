8cb0d0ea2aa532afdbf425633a2a93afa92abfb4
Mylene Febrissy <m.febrissy@gmail.com>
2022-09-12 10:25:20 +0200

    Add index, show, new with sff & bootstrap

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,4 +2,27 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animal_path(@animal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :address, :species)
+  end
 end
