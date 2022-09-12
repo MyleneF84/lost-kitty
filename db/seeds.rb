56a4c9fa4edb64102c4d4948c7fd560993622966
Mylene Febrissy <m.febrissy@gmail.com>
2022-09-12 09:55:20 +0200

    Create animal model, animals controller, seed & index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating animals...'
+rita = { name: 'Rita', address: 'Rue Condorcet', species: 'cat' }
+tobby = { name: 'Tobby', address: 'Rue des Martyrs', species: 'dog' }
+sparrow = { name: 'Sparrow', address: 'Rue de Verdun', species: 'bird' }
+
+[rita, tobby, sparrow].each { |args| animal = Animal.create!(args); puts "Created #{animal.name}" }
+
+puts 'Finished'
